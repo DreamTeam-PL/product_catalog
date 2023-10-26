fa6693c4fd1aa9045f25f4d4cfd93f028db9a6ec
Dolnys <thedolnys@gmail.com>
2023-10-26 17:43:56 +0200

    add scss, prepare file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Header } from './Components/Header';
-import { Body } from './Components/Body';
-import { Footer } from './Components/Footer';
+import { Header } from './Components/Header/Header';
+import { Body } from './Components/Body/Body';
+import { Footer } from './Components/Footer/Footer';
 
 export const App: React.FC = () => {
   return (

7446d336e5ff97e2e9b9dd9036fd0cc0cc22ca80
Dolnys <thedolnys@gmail.com>
2023-10-26 17:58:20 +0200

    reset scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './Styles/reset.scss'
 
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(<App />);

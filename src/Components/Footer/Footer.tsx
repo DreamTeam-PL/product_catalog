38b536a7c2922a00232ac230c4a7c546a5b6ebe4
AndrzejPec <pimatstudio@gmail.com>
2023-10-26 20:33:31 +0200

    got rid of wrong body styling

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,8 +3,12 @@ import './footer.scss';
 
 export const Footer = () => {
     return (
-        <footer className="footer-container">
-            <span className="brand-name">NICE & GADGETS</span>
+        <footer className="footer">
+            <img
+                className="footer__logo"
+                alt="Logo - footer"
+                src={require('./Logo.png')}
+            ></img>
             <div className="menu">
                 <span className="menu-item">GITHUB</span>
                 <span className="menu-item">CONTACTS</span>
@@ -12,7 +16,11 @@ export const Footer = () => {
             </div>
             <div className="back-to-top-container">
                 <span className="back-to-top">Back to top</span>
-                <span className="arrow">&uarr;</span>
+                <img
+                    className="arrow"
+                    alt="Back to top"
+                    src={require('./OK.png')}
+                ></img>
             </div>
         </footer>
     );

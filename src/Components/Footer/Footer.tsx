238c22a7d80e7852c764d57a85241dac801cb5ba
AndrzejPec <pimatstudio@gmail.com>
2023-10-26 19:39:46 +0200

    footer skeleton

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './footer.scss';
 
-export const Footer: React.FC = () => {
-  return (
-    <footer>
-    </footer>
-  );
+export const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <span className="brand-name">NICE & GADGETS</span>
+            <div className="menu">
+                <span className="menu-item">GITHUB</span>
+                <span className="menu-item">CONTACTS</span>
+                <span className="menu-item">RIGHTS</span>
+            </div>
+            <div className="back-to-top-container">
+                <span className="back-to-top">Back to top</span>
+                <span className="arrow">&uarr;</span>
+            </div>
+        </footer>
+    );
 };
+
+export default Footer;

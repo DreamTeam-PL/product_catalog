ef51f3ab39f9a4521d61a03750ca496861c17d34
Patryk Filipiak <patryk.fy@o2.pl>
2023-10-27 11:39:07 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Header } from './Components/Header'; 
-import { AppRouter } from './Components/AppRouter';
-import { Footer } from './Components/Footer';
-import './App.scss';
+import React from 'react'; 
+import { AppRouter } from './Components/AppRouter'; 
 
 export const App: React.FC = () => {
-  return (<>
-    <Router>
-      <Header />
-      <AppRouter />
-    </Router>
-    <Footer />
+  return (<>  
+    <AppRouter />
     </>);
 };
--- src/Components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => {
-  return (
-    <footer className='footer'>
-      Its Footer
-    </footer>
-  );
-};

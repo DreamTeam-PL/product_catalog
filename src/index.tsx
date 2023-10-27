1feaf8b6397655f68fd0461177adec0bfda41431
Patryk Filipiak <patryk.fy@o2.pl>
2023-10-27 11:59:14 +0200

    fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-  
-import './index.scss';
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App'
-import './Styles/reset.scss' 
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './Styles/reset.scss';
+import './Styles/fonts.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
     .render(

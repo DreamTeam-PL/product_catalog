7094c1d141bef40ca150d2ae27019ef7d6d9ddc5
Dolnys <thedolnys@gmail.com>
2023-10-27 12:10:47 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Header } from './Components/Header/Header';
-import { Body } from './Components/Body/Body';
-import { Footer } from './Components/Footer/Footer';
+import React from 'react'
+import { Header } from './Components/Header/Header'
+import { Body } from './Components/Body/Body'
+import { Footer } from './Components/Footer/Footer'
 
 export const App: React.FC = () => {
   return (
-    <div className="productCatalog">
+    <div className='productCatalog'>
       <Header />
       <Body />
       <Footer />
     </div>
-  );
-};
+  )
+}
--- src/Components/PhoneInfo/PhoneInfo.tsx
+++ src/Components/PhoneInfo/PhoneInfo.tsx
@@ -1,49 +1,81 @@
 import React from 'react'
+import { Circle } from './Circle'
 import './phoneinfo.scss'
+import { Capacity } from './Capacity'
 
 export const PhoneInfo: React.FC = () => {
   return (
-    <div className='phone-info'>
-      <h1 className='phone-info-title'>
+    <div className='phone__info'>
+      <h1 className='phone__info-title'>
         Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
       </h1>
 
-      <div>
-        <div className='phone-info-container'>
-          <div>
+      <div className='phone__info-container'>
+        <div className='phone__info-photo-container'>
+          <div className='phone__info-photo-content'>
             <img
-              className=''
-              src={require('../../Assets/Images/Iphone11.png')}
-              alt='Image'
-            />
-            <img
-              className=''
-              src={require('../../Assets/Images/Iphone11.png')}
-              alt='Image'
-            />
-            <img
-              className=''
-              src={require('../../Assets/Images/Iphone11.png')}
-              alt='Image'
-            />
-            <img
-              className=''
-              src={require('../../Assets/Images/Iphone11.png')}
-              alt='Image'
-            />
-            <img
-              className=''
+              className='phone__info-phone-photo phone__infophone-photo-small'
               src={require('../../Assets/Images/Iphone11.png')}
               alt='Image'
             />
           </div>
-          <div>
+          <div className='phone__info-photo-content'>
             <img
-              className=''
-              src={require('../../Assets/Images/Iphone_11.png')}
+              className='phone__info-phone-photo phone__infophone-photo-small'
+              src={require('../../Assets/Images/Iphone11.png')}
               alt='Image'
             />
           </div>
+          <div className='phone__info-photo-content'>
+            <img
+              className='phone__info-phone-photo phone__infophone-photo-small'
+              src={require('../../Assets/Images/Iphone11.png')}
+              alt='Image'
+            />
+          </div>
+          <div className='phone__info-photo-content'>
+            <img
+              className='phone__info-phone-photo phone__infophone-photo-small'
+              src={require('../../Assets/Images/Iphone11.png')}
+              alt='Image'
+            />
+          </div>
+          <div className='phone__info-photo-content'>
+            <img
+              className='phone__info-phone-photo phone__infophone-photo-small'
+              src={require('../../Assets/Images/Iphone11.png')}
+              alt='Image'
+            />
+          </div>
+        </div>
+        <div>
+          <img
+            className='phone-photo phone-photo-small'
+            src={require('../../Assets/Images/Iphone_11.png')}
+            alt='Image'
+          />
+        </div>
+        <div className='phone__info-main'>
+          <div className='phone__info-main-colors'>
+            <span className='phone__info-main-title'>Available colors</span>
+            <div className='phone__info-main-circles'>
+              <Circle fill='#FCDBC1' />
+              <Circle fill='#5F7170' />
+              <Circle fill='#4C4C4C' />
+              <Circle fill='#F0F0F0' />
+            </div>
+          </div>
+          <div className='phone__info-main-capacity'>
+            <span className='phone__info-main-title'>Select capacity</span>
+            <div className='phone__info-main-memory'>
+              <Capacity value='64GB' />
+              <Capacity value='128GB' />
+              <Capacity value='256GB' />
+            </div>
+          </div>
+
+          <div className='phone__info-main-price'></div>
+          <div className='phone__info-main-details'></div>
         </div>
       </div>
     </div>

8538ebd94e5d42cfaae0dc0244cd1b23cbf1dcf5
Patryk Filipiak <patryk.fy@o2.pl>
2023-10-27 16:25:57 +0200

    Slider & Title on Home

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -7,23 +7,23 @@ export const Slider:React.FC = () => {
     const [selected, setSelected] = useState<number>(1);
 
     return (
-    <section className={`slider slider--active-${selected}`}>
+    <section className='slider'>
         <a className='slider__button slider__button--prev'
             onClick={() => setSelected(current => current > 1 ? current - 1 : 3)}
         > </a>
         <main className={`slider__main slider__main--active-${selected}`}> 
             <a href="/link-to-buy">
-                <picture className="slider__picture">
-                    <source
-                        media="(max-width: 639px)"
-                        srcSet={require('./image_small.png')}
-                    />
-                    <img
-                        className='slider__image'
-                        src={require('./image_big.png')}
-                        alt="img1"
-                    />
-                </picture>
+            <picture className="slider__picture">
+                <source
+                    media="(max-width: 639px)"
+                    srcSet={require('./image_small.png')}
+                />
+                <img
+                    className='slider__image'
+                    src={require('./image_big.png')}
+                    alt="img1"
+                />
+            </picture>
             </a>
         </main>
         <a className='slider__button slider__button--next'

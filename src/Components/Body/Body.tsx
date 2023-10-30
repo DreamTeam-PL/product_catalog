6a1daa7e6cadc1c260a4cb3e73837d32f5dcbf0a
Dolnys <thedolnys@gmail.com>
2023-10-30 15:23:24 +0100

    configure git pages

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import { PhoneInfo } from '../PhoneInfo/PhoneInfo'
 
 export const Body: React.FC = () => {
-  return (
-    <body>
-      <PhoneInfo />
-    </body>
-  )
+  return <p>lol</p>
 }

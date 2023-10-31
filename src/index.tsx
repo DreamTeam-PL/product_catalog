4bda745917415ae7bd48afeafea31358caf2ff08
Kamil <majcherczak.kamil@protonmail.com>
2023-10-31 15:20:46 +0100

    change BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './Styles/reset.scss';
 import './Styles/fonts.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
     .render(

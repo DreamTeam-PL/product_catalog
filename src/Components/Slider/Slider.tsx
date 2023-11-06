3c7c46ed2b349ac657380bac4084abcfdcb54315
Dolnys <thedolnys@gmail.com>
2023-11-06 14:59:24 +0100

    add slider photos

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -1,34 +1,53 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { useState } from 'react';
-import './Slider.scss'; 
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import './Slider.scss'
+import { Link } from 'react-router-dom'
 
+export const Slider: React.FC = () => {
+  const [selected, setSelected] = useState<number>(1)
 
-export const Slider:React.FC = () => {
-    const [selected, setSelected] = useState<number>(1);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSelected((current) => (current < 3 ? current + 1 : 1))
+    }, 10000)
 
-    return (
+    return () => {
+      clearInterval(timer)
+    }
+  }, [selected])
+
+  const handlePrevClick = () => {
+    setSelected((current) => (current > 1 ? current - 1 : 3))
+  }
+
+  const handleNextClick = () => {
+    setSelected((current) => (current < 3 ? current + 1 : 1))
+  }
+
+  return (
     <section className={`slider slider--active-${selected}`}>
-        <a className='slider__button slider__button--prev'
-            onClick={() => setSelected(current => current > 1 ? current - 1 : 3)}
-        > </a>
-        <main className={`slider__main slider__main--active-${selected}`}> 
-            <Link to="/phones/1">
-                <picture className="slider__picture">
-                    <source
-                        media="(max-width: 639px)"
-                        srcSet={require('./image_small.png')}
-                    />
-                    <img
-                        className='slider__image'
-                        src={require('./image_big.png')}
-                        alt="img1"
-                    />
-                </picture>
-            </Link>
-        </main>
-        <a className='slider__button slider__button--next'
-        onClick={() => setSelected(current => current < 3 ? current + 1 : 1)}
-        > </a>
-    </section>);
+      <a
+        className='slider__button slider__button--prev'
+        onClick={handlePrevClick}
+      ></a>
+      <main className={`slider__main slider__main--active-${selected}`}>
+        <Link to={`/phones/${selected}`}>
+          <picture className='slider__picture'>
+            <source
+              media='(max-width: 639px)'
+              srcSet={require(`./image_small${selected}.png`)}
+            />
+            <img
+              className='slider__image'
+              src={require(`./image_big${selected}.png`)}
+              alt={`img${selected}`}
+            />
+          </picture>
+        </Link>
+      </main>
+      <a
+        className='slider__button slider__button--next'
+        onClick={handleNextClick}
+      ></a>
+    </section>
+  )
 }

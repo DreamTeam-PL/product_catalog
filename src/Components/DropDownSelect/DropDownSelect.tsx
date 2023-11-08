7c3a46642c986c4b7d7b23cd92d55b75074e3018
AndrzejPec <pimatstudio@gmail.com>
2023-11-08 12:10:56 +0100

    Added select list close on click outside the menu

--- src/Components/DropDownSelect/DropDownSelect.tsx
+++ src/Components/DropDownSelect/DropDownSelect.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './DropDownSelect.scss';
 
 type Props = {
     options: string[];
-    onChange: ( option: number ) => void;
-    label?:string;
-    className?:string; 
+    onChange: (option: number) => void;
+    label?: string;
+    className?: string;
     selected: number;
+    focus: boolean;
+    setFocus: (focus: boolean) => void;
 };
 
-export const DropDownSelect:React.FC<Props> = ({ options, onChange, label, className, selected}) => {
+export const DropDownSelect: React.FC<Props> = ({ options, onChange, label, className, selected, focus, setFocus }) => {
     console.log('Init selected: ', selected)
     const [selectedOption, setSelectedOption] = useState<number>(selected || 0);
-    const [focus, setFocus] = useState<boolean>(false);
 
-    return (<div className={`dropDown ${className ? className : '' }`}>
+    return (<div className={`dropDown ${className ? className : ''}`}>
         {label && <p className='dropDown__label'> {label} </p>}
         <p
-            className={`dropDown__selected ${focus ? 'dropDown__selected--focused': ''}`}
+            className={`dropDown__selected ${focus ? 'dropDown__selected--focused' : ''}`}
             onClick={() => setFocus(!focus)}
         >
-            {   options[selectedOption]}
+            {options[selectedOption]}
         </p>
 
         {focus && (<ul className='dropDown__options'>
             {options.map((option, key) => <li
                 className='dropDown__option'
-                key={option} 
+                key={option}
                 onClick={() => {
                     setSelectedOption(key);
                     setFocus(false);
@@ -35,6 +36,6 @@ export const DropDownSelect:React.FC<Props> = ({ options, onChange, label, class
             >
                 {option}
             </li>)} 
-        </ul>)} 
+        </ul>)}
     </div>);
 }
--- src/Components/ProductCatalog/ProductCatalog.tsx
+++ src/Components/ProductCatalog/ProductCatalog.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Product } from "../../types/types";
 import { Card } from "../Card/Card";
 import { DropDownSelect } from "../DropDownSelect/DropDownSelect";
@@ -5,32 +6,58 @@ import './ProductCatalog.scss';
 
 
 
-type Props = { 
+type Props = {
     products: Product[];
     filters?: {
         label?: string;
         options: string[];
-        onChange: (option:number) => void;
+        onChange: (option: number) => void;
         selected?: number;
     }[];
-} 
+}
 
-export const ProductCatalog:React.FC<Props> = ({ products, filters = [] }) => {
+export const ProductCatalog: React.FC<Props> = ({ products, filters = [] }) => {
+    const [focusStates, setFocusStates] = useState(filters.map(() => false));
+
+    const handleGlobalClick = (index: number) => () => {
+        const updatedFocusStates = [...focusStates];
+        updatedFocusStates[index] = false;
+        setFocusStates(updatedFocusStates);
+    };
+
+    useEffect(() => {
+        filters.forEach((_, i) => {
+            document.addEventListener('mousedown', handleGlobalClick(i));
+        });
+
+        return () => {
+            filters.forEach((_, i) => {
+                document.removeEventListener('mousedown', handleGlobalClick(i));
+            });
+        };
+    }, [filters]);
+
     return (
-    <section className="product-catalog"> 
-        <section className="product-catalog__filters">
-            {filters.map((filter, i) => (<DropDownSelect
-                key={filter.label || i.toString()}
-                className="product-catalog__filter"
-                label={filter.label}
-                options={filter.options}
-                onChange={filter.onChange}  
-                selected={filter.selected || 0}
-            />  ))} 
-        </section>
+        <section className="product-catalog">
+            <section className="product-catalog__filters">
+                {filters.map((filter, i) => (<DropDownSelect
+                    key={filter.label || i.toString()}
+                    className="product-catalog__filter"
+                    label={filter.label}
+                    options={filter.options}
+                    onChange={filter.onChange}
+                    selected={filter.selected || 0}
+                    focus={focusStates[i]}
+                    setFocus={(focus) => {
+                        const updatedFocusStates = [...focusStates];
+                        updatedFocusStates[i] = focus;
+                        setFocusStates(updatedFocusStates);
+                    }}
+                />))}
+            </section>
 
-        <main className="product-catalog__grid">
-            {products.map(product => (<Card key={product.id} item={product} />))}
-        </main>
-    </section>);
+            <main className="product-catalog__grid">
+                {products.map(product => (<Card key={product.id} item={product} />))}
+            </main>
+        </section>);
 }
